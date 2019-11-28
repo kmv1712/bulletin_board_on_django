abe5e61603c9c682ad24da90f8345cd1240d8545
KarpovM <kmv1712@gmail.com>
2019-11-28 09:52:09 +0500

    Почитал про методы objects до стр.42
    objects.all()
    objects.filter()
    objects.order_by()
    objects.delete()
    objects.get()
    objects.create()

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Bd
+
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений")
+    s = 'Список объявлений\r\n\r\n\r\n'
+    for bd in Bd.objects.order_by('-published'):
+        s += bd.title + '\r\n' + bd.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
 # Create your views here.
--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from bboard.views import index
-
 urlpatterns = [
-    path('bboard', include('bboard.urls')),
+    path('bboard/', include('bboard.urls')),
     path('admin/', admin.site.urls),
 ]
 
